02e8ba28ce866cd6e608a5c1862a63f8b3a90a92
I7RANK <franfgh2000@hotmail.com>
2020-10-19 23:43:53 +0000

    refactoring

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,50 @@
 #include "sort.h"
 
+void sort_part(int *arr, size_t size, size_t i_low, size_t pivot);
+
+/**
+ * refac - function to refactor for betty warnings
+ * @arr: array
+ * @size: array size
+ * @i_low: i_low
+ * @pivot: pivot
+ * @i: i
+ *
+ * Return: na
+*/
+void refac(int *arr, size_t size, size_t *i_low, size_t *pivot, size_t *i)
+{
+	size_t save_i = 0, j;
+	int temp = 0;
+
+	save_i = *i;
+	for (j = *i + 1; j <= *pivot; j++)
+	{
+		if (arr[j] < arr[*pivot] || j == *pivot)
+		{
+			/* if equals numbers no swap */
+			if (arr[j] != arr[save_i])
+			{
+				temp = arr[save_i];
+				arr[save_i] = arr[j];
+				arr[j] = temp;
+				print_array(arr, size);
+			}
+			if (j == *pivot)
+			{
+				if (save_i == *i_low)
+					(*i_low)++;
+				else
+					(*pivot)--;
+				if (*pivot == 0)
+					return;
+				sort_part(arr, size, *i_low, *pivot);
+			}
+			save_i++;
+		}
+	}
+}
+
 /**
  * sort_part - sorts a number list in recursive mode
  * @arr: the array with number to sort
@@ -11,38 +56,13 @@
 */
 void sort_part(int *arr, size_t size, size_t i_low, size_t pivot)
 {
-	size_t save_i = 0, i, j;
-	int temp = 0;
+	size_t i;
 
 	for (i = i_low; i <= pivot; i++)
 	{
 		if (arr[i] >= arr[pivot])
 		{
-			save_i = i;
-			for (j = i + 1; j <= pivot; j++)
-			{
-				if (arr[j] < arr[pivot] || j == pivot)
-				{
-					temp = arr[save_i];
-					arr[save_i] = arr[j];
-					arr[j] = temp;
-					/* if swap equals numbers no print */
-					if (arr[j] != arr[save_i])
-						print_array(arr, size);
-					if (j == pivot)
-					{
-					/* no indent betty warnings */
-					if (save_i == i_low)
-						i_low++;
-					else
-						pivot--;
-					if (pivot == 0)
-						return;
-					sort_part(arr, size, i_low, pivot);
-					}
-					save_i++;
-				}
-			}
+			refac(arr, size, &i_low, &pivot, &i);
 		}
 	}
 	/* pivot must be equal to i_low for the array is sorted */
