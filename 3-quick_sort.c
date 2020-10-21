18ba6faaca3fbe15c2251a89dd051a2630ca49a2
I7RANK <franfgh2000@hotmail.com>
2020-10-21 01:51:39 +0000

    remake task 3 again

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,47 +8,37 @@ void sort_part(int *arr, size_t size, size_t i_low, size_t pivot);
  * @arr: array
  * @size: array size
  * @i_low: index low
- * @i_hight: index hight
+ * @pivot: pivot
  *
  * Return: pivot
 */
-size_t partition(int *arr, size_t size, size_t i_low, size_t i_hight)
+size_t partition(int *arr, size_t size, size_t i_low, size_t pivot)
 {
+	size_t i, save_i = i_low;
 	int temp = 0;
-	size_t save_i = 0, i, j, sort = 1;
 
-	for (i = i_low; i <= i_hight; i++)
+	for (i = i_low; i < pivot; i++)
 	{
-		if (arr[i] > arr[i_hight])
+		if (arr[i] < arr[pivot])
 		{
-			save_i = i;
-			for (j = i + 1; j < i_hight; j++)
+			if (i != save_i)
 			{
-				if (arr[j] < arr[i_hight])
-				{
-					temp = arr[j];
-					arr[j] = arr[save_i];
-					arr[save_i] = temp;
-					print_array(arr, size);
-					save_i++;
-				}
+				temp = arr[i];
+				arr[i] = arr[save_i];
+				arr[save_i] = temp;
+				print_array(arr, size);
 			}
-			temp = arr[i_hight];
-			arr[i_hight] = arr[save_i];
-			arr[save_i] = temp;
-			print_array(arr, size);
-			return (save_i);
+			save_i++;
 		}
 	}
-	for (i = 0; i < size - 1; i++)
+	if (arr[save_i] > arr[pivot])
 	{
-		if (arr[i] <= arr[i + 1])
-			sort++;
+		temp = arr[save_i];
+		arr[save_i] = arr[pivot];
+		arr[pivot] = temp;
+		print_array(arr, size);
 	}
-	if (sort == size)
-		return (0);
-	sort_part(arr, size, i_low, i_hight - 1);
-	return (0);
+	return (save_i);
 }
 
 /**
@@ -62,15 +52,7 @@ size_t partition(int *arr, size_t size, size_t i_low, size_t i_hight)
 */
 void sort_part(int *arr, size_t size, size_t i_low, size_t i_hight)
 {
-	size_t pivot = 0, sort = 1, i;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		if (arr[i] <= arr[i + 1])
-			sort++;
-	}
-	if (sort == size)
-		return;
+	size_t pivot = 0;
 
 	if (i_low < i_hight)
 	{
@@ -79,17 +61,9 @@ void sort_part(int *arr, size_t size, size_t i_low, size_t i_hight)
 		{
 			sort_part(arr, size, i_low, pivot - 1);
 		}
-		else
+		if (pivot < size - 1)
 		{
 			sort_part(arr, size, pivot + 1, i_hight);
-			for (i = 0; i < size - 1; i++)
-			{
-				if (arr[i] <= arr[i + 1])
-					sort++;
-			}
-			if (sort == size)
-				return;
-			sort_part(arr, size, 0, size - 1);
 		}
 	}
 }
